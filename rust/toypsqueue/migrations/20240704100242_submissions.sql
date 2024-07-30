9bc4bce4119bd7c3ecf0f9b90e4ac2e8db7a8e28
Marten Wijnja <marten.wijnja@channable.com>
2024-07-30 16:14:38 +0200

    Change tables to three-table format (x, x_completed, x_failed)

--- rust/toypsqueue/migrations/20240704100242_submissions.sql
+++ rust/toypsqueue/migrations/20240704100242_submissions.sql
@@ -6,3 +6,20 @@ CREATE TABLE submissions
     chunks_done INTEGER NOT NULL DEFAULT 0,
     metadata BLOB
 ) STRICT;
+
+CREATE TABLE submissions_completed
+(
+    id INTEGER PRIMARY KEY NOT NULL,
+    chunks_total INTEGER NOT NULL DEFAULT 0,
+    metadata BLOB,
+    completed_at INTEGER NOT NULL -- Unix Timestamp
+) STRICT;
+
+CREATE TABLE submissions_failed
+(
+    id INTEGER PRIMARY KEY NOT NULL,
+    chunks_total INTEGER NOT NULL DEFAULT 0,
+    metadata BLOB,
+    failed_chunk_id INTEGER NOT NULL, -- NOTE: There might in rare cases be multiple concurrent failures, but only one of them will be 'first' (ordered by who gets write access to the submissions table lock first), which is the one stored here.
+    failed_at INTEGER NOT NULL -- Unix Timestamp
+) STRICT;
--- rust/toypsqueue/migrations/20240704100449_chunks.sql
+++ rust/toypsqueue/migrations/20240704100449_chunks.sql
@@ -1,9 +1,31 @@
--- Add migration script here
 CREATE TABLE chunks
 (
     submission_id INTEGER NOT NULL,
     id INTEGER NOT NULL,
-    uri BLOB NOT NULL DEFAULT "",
+    input_content BLOB NOT NULL,
+    retries INTEGER NOT NULL,
+
+    PRIMARY KEY (submission_id, id)
+) STRICT;
+
+CREATE TABLE chunks_completed
+(
+    submission_id INTEGER NOT NULL,
+    id INTEGER NOT NULL,
+    output_content BLOB NOT NULL,
+    completed_at INTEGER NOT NULL, -- Unix Timestamp
+
+    PRIMARY KEY (submission_id, id)
+) STRICT;
+
+
+CREATE TABLE chunks_failed
+(
+    submission_id INTEGER NOT NULL,
+    id INTEGER NOT NULL,
+    input_content BLOB NOT NULL,
+    failure BLOB NOT NULL,
+    failed_at INTEGER NOT NULL, -- Unix Timestamp
 
     PRIMARY KEY (submission_id, id)
 ) STRICT;
