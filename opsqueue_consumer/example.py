5bfde6290557e64079aa53859bb97eb340ce381f
Marten Wijnja <marten.wijnja@channable.com>
2024-10-22 14:58:56 +0200

    Alter config of the 'no-code-in-__init__.py' linter,
    so it doesn't complain about Python files in packages in the nested `.venv`
    that is (only) used by `maturin develop`

--- opsqueue_consumer/example.py
+++ opsqueue_consumer/example.py
@@ -5,10 +5,10 @@ from opsqueue_consumer import Client, Strategy, Chunk
 def myfun(chunk: Chunk) -> bytes:
     # time.sleep(1) # We simulate that this takes a while
     # return 1 / 0
-    if chunk.input_content is None:
-        return b"Hello world!"
-    else:
-        return chunk.input_content + b"Was altered!"
+    # if chunk.input_content is None:
+    #     return b"Hello world!"
+    # else:
+    return chunk.input_content + b"Was altered!"
 
 
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
