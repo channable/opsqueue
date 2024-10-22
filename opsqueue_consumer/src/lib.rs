ec89dec6682d459587bb97013ed72a29f8a788d0
Marten Wijnja <marten.wijnja@channable.com>
2024-10-22 12:41:45 +0200

    Add .envrc to the producer/consumer libs so we can run `maturin develop` again even inside Nix
    (This is because `maturin develop` expects a Python virtual env, so we
    need an empty one)

--- opsqueue_consumer/src/lib.rs
+++ opsqueue_consumer/src/lib.rs
@@ -59,7 +59,7 @@ impl Client {
         submission_id: SubmissionId,
         submission_prefix: Option<String>,
         chunk_index: ChunkIndex,
-        output_content: chunk::Content,
+        output_content: Vec<u8>,
     ) -> PyResult<()> {
         py.allow_threads(|| { self.complete_chunk_gilless(submission_id, submission_prefix, chunk_index, output_content)})
     }
@@ -69,7 +69,7 @@ impl Client {
         &self,
         py: Python<'_>,
         submission_id: SubmissionId,
-        submission_prefix: Option<&str>,
+        submission_prefix: Option<String>,
         chunk_index: ChunkIndex,
         failure: String,
     ) -> PyResult<()> {
@@ -91,23 +91,23 @@ impl Client {
                     log::debug!("Reserved {} chunks", chunks.len());
                     for chunk in chunks {
                         let submission_id = chunk.submission_id;
-                        let submission_prefix = chunk.submission_prefix;
+                        let submission_prefix = chunk.submission_prefix.clone();
                         let chunk_index = chunk.chunk_index;
-                        log::debug!("Running fun for chunk: submission_id={:?}, chunk_index={:?}, submission_prefix={:?}", submission_id, chunk_index, submission_prefix);
+                        log::debug!("Running fun for chunk: submission_id={:?}, chunk_index={:?}, submission_prefix={:?}", submission_id, chunk_index, &submission_prefix);
                         let res = Python::with_gil(|py| {
                             let res = unbound_fun.bind(py).call1((chunk,))?;
                             res.extract()
                         });
                         match res {
                             Ok(res) => {
-                                log::debug!("Completing chunk: submission_id={:?}, chunk_index={:?}, submission_prefix={:?}", submission_id, chunk_index, submission_prefix);
-                                self.complete_chunk_gilless(submission_id, chunk.submission_prefix, chunk_index, res)?;
-                                log::debug!("Completed chunk: submission_id={:?}, chunk_index={:?}, submission_prefix={:?}", submission_id, chunk_index, submission_prefix);
+                                log::debug!("Completing chunk: submission_id={:?}, chunk_index={:?}, submission_prefix={:?}", submission_id, chunk_index, &submission_prefix);
+                                self.complete_chunk_gilless(submission_id, submission_prefix.clone(), chunk_index, res)?;
+                                log::debug!("Completed chunk: submission_id={:?}, chunk_index={:?}, submission_prefix={:?}", submission_id, chunk_index, &submission_prefix);
                             },
                             Err(failure) => {
-                                log::warn!("Failing chunk: submission_id={:?}, chunk_index={:?}, submission_prefix={:?}, reason: {failure:?}", submission_id, chunk_index, submission_prefix);
-                                self.fail_chunk_gilless(submission_id, chunk.submission_prefix, chunk_index, format!("{failure:?}"))?;
-                                log::warn!("Failed chunk: submission_id={:?}, chunk_index={:?}, submission_prefix={:?}", submission_id, chunk_index, submission_prefix);
+                                log::warn!("Failing chunk: submission_id={:?}, chunk_index={:?}, submission_prefix={:?}, reason: {failure:?}", submission_id, chunk_index, &submission_prefix);
+                                self.fail_chunk_gilless(submission_id, submission_prefix.clone(), chunk_index, format!("{failure:?}"))?;
+                                log::warn!("Failed chunk: submission_id={:?}, chunk_index={:?}, submission_prefix={:?}", submission_id, chunk_index, &submission_prefix);
                                 // On exceptions that are not PyExceptions (but PyBaseExceptions), like KeyboardInterrupt etc, return.
                                 // otherwise, continue with next chunk
                                 if !Python::with_gil(|py| {failure.is_instance_of::<PyException>(py)}) {
@@ -187,15 +187,15 @@ impl Client {
         submission_id: SubmissionId,
         submission_prefix: Option<String>,
         chunk_index: ChunkIndex,
-        output_content: chunk::Content,
+        output_content: Vec<u8>,
     ) -> PyResult<()> {
         let chunk_id = (submission_id.into(), chunk_index.into());
         self.block_unless_interrupted(async move {
             match submission_prefix {
                 None =>
-                    self.client.complete_chunk(chunk_id, output_content).await?,
+                    self.client.complete_chunk(chunk_id, Some(output_content)).await,
                 Some(prefix) => {
-                    self.object_store_client.store_chunk(submission_prefix, chunk_index, ChunkType::Output, output_content).await?;
+                    self.object_store_client.store_chunk(&prefix, chunk_id.1, ChunkType::Output, output_content).await?;
                     self.client.complete_chunk(chunk_id, None).await
                 }
             }
@@ -206,7 +206,7 @@ impl Client {
     pub fn fail_chunk_gilless(
         &self,
         submission_id: SubmissionId,
-        submission_prefix: Option<String>,
+        _submission_prefix: Option<String>,
         chunk_index: ChunkIndex,
         failure: String,
     ) -> PyResult<()> {
@@ -356,14 +356,14 @@ pub struct Chunk {
 
 impl Chunk {
     pub async fn from_internal(c: chunk::Chunk, s: submission::Submission, object_store_client: &ObjectStoreClient) -> anyhow::Result<Self> {
-        let content = match c.input_content {
-            Some(bytes) => bytes,
+        let (content, prefix) = match c.input_content {
+            Some(bytes) => (bytes, None),
             None => {
                 let prefix = s.prefix.unwrap();
                 log::debug!("Fetching chunk content from object store: submission_id={}, prefix={}, chunk_index={}", c.submission_id, prefix, c.chunk_index);
                 let res = object_store_client.retrieve_chunk(&prefix, c.chunk_index, ChunkType::Input).await?.to_vec();
                 log::debug!("Fetched chunk content: {res:?}");
-                res
+                (res, Some(prefix))
             }
         };
         Ok(Chunk {
@@ -371,7 +371,7 @@ impl Chunk {
             chunk_index: c.chunk_index.into(),
             input_content: ContentAsBytes(content),
             retries: c.retries,
-            submission_prefix: s.prefix,
+            submission_prefix: prefix,
             submission_metadata: s.metadata,
         })
     }
