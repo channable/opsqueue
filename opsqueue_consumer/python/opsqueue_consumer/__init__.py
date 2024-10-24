7a80f50a59428d45ac9eff7d45dc48b3c55a918d
Marten Wijnja <marten.wijnja@channable.com>
2024-10-24 12:59:08 +0200

    Document the consumer client python functions

--- opsqueue_consumer/python/opsqueue_consumer/__init__.py
+++ opsqueue_consumer/python/opsqueue_consumer/__init__.py
@@ -23,6 +23,7 @@ class json_as_bytes:
 
 
 DEFAULT_SERIALIZATION_FORMAT: SerializationFormat = json_as_bytes
+DEFAULT_STRATEGY = Strategy.Newest
 
 
 class Client:
@@ -35,7 +36,57 @@ class Client:
     def __init__(self, opsqueue_url: str, object_store_url: str):
         self.inner = opsqueue_consumer_internal.Client(opsqueue_url, object_store_url)  # type: ignore[name-defined] # noqa: F821
 
-    def reserve_chunks(self, max: int, strategy: Strategy) -> list[Chunk]:
+    def run_each_op(
+        self,
+        op_callback: Callable[[Chunk], bytes],
+        *,
+        strategy: Strategy = DEFAULT_STRATEGY,
+        serialization_format: SerializationFormat = DEFAULT_SERIALIZATION_FORMAT,
+    ):
+        """
+        Runs the given `op_callback` for each reservable operation in a loop.
+
+        This function blocks 'forever', except when 'special' exceptions like KeyboardInterrupt are raised.
+        Specifically, normal exceptions (inheriting from `Exception`) will be caught and cause
+        `fail_chunk` to be called, with the loop afterwards continuing.
+        Exceptions inheriting only from `BaseException` will cause the loop to terminate.
+        """
+
+        def chunk_callback(c: Chunk) -> bytes:
+            ops = _decode_chunk(c.input_content, serialization_format)
+            ops_results = [op_callback(op) for op in ops]
+            return _encode_chunk(ops_results, serialization_format)
+
+        self.run_each_chunk_raw(chunk_callback, strategy=strategy)
+
+    # TODO: run_each_chunk
+
+    def run_each_chunk_raw(
+        self,
+        chunk_callback: Callable[[Chunk], bytes],
+        *,
+        strategy: Strategy = DEFAULT_STRATEGY,
+        serialization_format: SerializationFormat = DEFAULT_SERIALIZATION_FORMAT,
+    ):
+        """
+        Runs the given `chunk_callback` for each chunk the consumer can reserve in a loop.
+        This expects encoding/decoding of the chunk contents from/to bytes to be done manually by you.
+
+        This function blocks 'forever', except when 'special' exceptions like KeyboardInterrupt are raised.
+        Specifically, normal exceptions (inheriting from `Exception`) will be caught and cause
+        `fail_chunk` to be called, with the loop afterwards continuing.
+        Exceptions inheriting only from `BaseException` will cause the loop to terminate.
+        """
+        self.inner.run_per_chunk(strategy, chunk_callback)  # type: ignore[no-any-return]
+    def reserve_chunks(self, max: int = 1, strategy: Strategy = Strategy.Newest) -> list[Chunk]:
+        """
+        Low-level function to manually reserve one or more chunks for processing.
+
+        Reserved chunks need to individually be marked as completed or failed
+        using `complete_chunk` resp. `fail_chunk`.
+
+        If your code crashes, all reserved chunks will automatically be marked as failed.
+        """
         self.inner.reserve_chunks(max, strategy)  # type: ignore[no-any-return]
 
     def complete_chunk(
@@ -45,6 +96,13 @@ class Client:
         chunk_index: int,
         output_content: bytes,
     ):
+        """
+        Low-level function to manually mark a chunk as completed,
+        passing the output content bytes.
+
+        The submission_id, submission_prefix and chunk_index can be found
+        on the `Chunk` type originally received as part of `reserve_chunks`.
+        """
         self.inner.complete_chunk(
             submission_id, submission_prefix, chunk_index, output_content
         )  # type: ignore[no-any-return]
@@ -56,32 +114,17 @@ class Client:
         chunk_index: int,
         failure: str,
     ):
+        """
+        Low-level function to manually mark a chunk as completed,
+        passing the failure message as a string.
+
+        This failure message is meant for developer eyes, i.e. it should be a
+        pretty-printed exception message and possibly its stack trace.
+
+        The submission_id, submission_prefix and chunk_index can be found
+        on the `Chunk` type originally received as part of `reserve_chunks`.
+        """
         self.inner.fail_chunk(submission_id, submission_prefix, chunk_index, failure)  # type: ignore[no-any-return]
-
-    def run_each_op(
-        self,
-        strategy: Strategy,
-        op_callback: Callable[[Chunk], bytes],
-        *,
-        serialization_format: SerializationFormat = DEFAULT_SERIALIZATION_FORMAT,
-    ):
-        def chunk_callback(c: Chunk) -> bytes:
-            ops = _decode_chunk(c.input_content, serialization_format)
-            ops_results = [op_callback(op) for op in ops]
-            return _encode_chunk(ops_results, serialization_format)
-
-        self.run_each_chunk_raw(strategy, chunk_callback)
-
-    # TODO: run_each_chunk
-
-    def run_each_chunk_raw(
-        self,
-        strategy: Strategy,
-        chunk_callback: Callable[[Chunk], bytes],
-        *,
-        serialization_format: SerializationFormat = DEFAULT_SERIALIZATION_FORMAT,
-    ):
-        self.inner.run_per_chunk(strategy, chunk_callback)  # type: ignore[no-any-return]
 
 
 def _encode_chunk(
