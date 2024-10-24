19a59d570e255f628690c37fbb5aab4f96aee878
Marten Wijnja <marten.wijnja@channable.com>
2024-10-24 15:35:39 +0200

    Remove 'pickle' as serialization format in the example again,
    (that was just there to show the AI team)

--- opsqueue_consumer/example.py
+++ opsqueue_consumer/example.py
@@ -1,14 +1,12 @@
 import logging
 from opsqueue_consumer import Client, Strategy
-import pickle
 
-logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.DEBUG)
+logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
 
 
 def my_operation(data: int) -> int:
-    # print(data)
     return data + 1
 
 
 client = Client("ws://localhost:3998", "file:///tmp/opsqueue/")
-client.run_each_op(my_operation, strategy=Strategy.Newest, serialization_format=pickle)
+client.run_each_op(my_operation, strategy=Strategy.Newest)
--- opsqueue_producer/example.py
+++ opsqueue_producer/example.py
@@ -1,16 +1,13 @@
 # It's recommended to just experiment with this in ipython
 import logging
 from opsqueue_producer import Client
-import pickle
 
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
 
 client = Client("localhost:3999", "file:///tmp/opsqueue/")
 
 input_iter = range(0, 100_000)
-output_iter = client.run_submission(
-    input_iter, chunk_size=100, serialization_format=pickle
-)
+output_iter = client.run_submission(input_iter, chunk_size=100)
 
 # Now do something with the output:
 for x in output_iter:
