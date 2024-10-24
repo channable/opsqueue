51fd24ed6e356cea913cbf0603fa2a8dfa384adf
Marten Wijnja <marten.wijnja@channable.com>
2024-10-24 10:14:33 +0200

    Reformat producer example :-)

--- opsqueue_producer/example.py
+++ opsqueue_producer/example.py
@@ -1,10 +1,10 @@
 # It's recommended to just experiment with this in ipython
 import logging
-from opsqueue_producer import Client, json_as_bytes
+from opsqueue_producer import Client
 
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
 client = Client("localhost:3999", "file:///tmp/opsqueue/")
 # result = client.run_submission_chunks(map(lambda x: str(x).encode(), range(0, 1_000)))
-result = client.run_submission(range(0, 1_000_000), chunk_size = 10000)
+result = client.run_submission(range(0, 1_000_000), chunk_size=10000)
 for x in result:
     print(x)
