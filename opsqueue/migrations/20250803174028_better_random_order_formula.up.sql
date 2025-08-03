689ccfc6928875a2e2cab69e1635212e193b7f97
Marten Wijnja <marten.wijnja@channable.com>
2025-08-03 19:58:12 +0200

    Alter the chunk virtual column `random_order` to be more fair
    Before, only the last 16 bits of the submission_id were used
    which were in practice nearly always `0`, since there was never more
    than one submission being generated in the same millisecond (c.f. how
    Snowflake IDs work).
    
    Now, also the higher 64-22=42 bits making up the timestamp
    are directly involved in the random order, which results
    in chunks with the same index in different submissions getting
    wildly different random order numbers.

---
+++ opsqueue/migrations/20250803174028_better_random_order_formula.up.sql
@@ -0,0 +1,38 @@
+-- 1. Drop all indexes and finally the column itself
+DROP INDEX random_chunks_order;
+DROP INDEX random_chunks_metadata_order;
+DROP INDEX random_chunks_metadata_order2;
+ALTER TABLE chunks DROP COLUMN random_order;
+ALTER TABLE chunks_metadata DROP COLUMN random_order;
+
+-- 2. Recreate the column with its new, proper definition
+ALTER TABLE chunks ADD COLUMN random_order INTEGER NOT NULL GENERATED ALWAYS AS (
+    (((submission_id + (submission_id >> 22) + chunk_index) % 65536) * 40503) % 65536
+    ) VIRTUAL;
+ALTER TABLE chunks_metadata ADD COLUMN random_order INTEGER NOT NULL GENERATED ALWAYS AS (
+    (((submission_id + (submission_id >> 22) + chunk_index) % 65536) * 40503) % 65536
+) VIRTUAL;
+
+
+-- 3. Recreate all dropped indexes
+CREATE INDEX random_chunks_order ON chunks (
+      random_order
+    , submission_id
+    , chunk_index
+);
+
+CREATE INDEX random_chunks_metadata_order ON chunks_metadata (
+      metadata_key
+    , metadata_value
+    , random_order
+    , submission_id
+    , chunk_index
+);
+
+CREATE INDEX random_chunks_metadata_order2 ON chunks_metadata (
+      metadata_key
+    , random_order
+    , metadata_value
+    , submission_id
+    , chunk_index
+);
