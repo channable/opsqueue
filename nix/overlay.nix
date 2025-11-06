1795986455a4db5a9eacb86135850d1d969323fc
Daan Rijks <daan.rijks@channable.com>
2025-11-06 18:44:24 +0100

    Use the final-prev convention for overlays instead of self-super

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,5 +1,5 @@
 # Overlay for Nixpkgs which holds all opsqueue related packages.
-self: super:
+final: prev:
 let
   sources = import ./sources.nix;
   pythonOverlay = import ./python-overlay.nix { inherit sources; };
@@ -7,19 +7,19 @@ in
 {
   # Placing the sources in the overlay gives all packages access to the sources,
   # and it makes it possible to override them in new overlays.
-  sources = if super ? sources then super.sources // sources else sources;
+  sources = if prev ? sources then prev.sources // sources else sources;
 
-  opsqueue = self.callPackage ../opsqueue/opsqueue.nix { };
+  opsqueue = final.callPackage ../opsqueue/opsqueue.nix { };
 
   # The explicit choice is made not to override `python312`, as this will cause a rebuild of many
   # packages when nixpkgs uses python 3.12 as default python environment.
   # These packages should not be affected, e.g. cachix. This is because of a transitive
   # dependency on the Python packages that we override.
   # In our case cachix > ghc > shpinx > Python libraries.
-  pythonChannable = super.python312.override { packageOverrides = pythonOverlay; };
+  pythonChannable = prev.python312.override { packageOverrides = pythonOverlay; };
 
   # We choose a minimal Rust channel to keep the Nix closure size smaller
-  rust-with-lsp = self.rust-bin.stable.latest.minimal.override {
+  rust-with-lsp = final.rust-bin.stable.latest.minimal.override {
     extensions = [
       "clippy"
       "rustfmt"
